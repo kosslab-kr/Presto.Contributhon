7a3439a68c8fb5818fc7ccee79af6fef1fce8ee7
Jo Tae Sang <gsts007@gmail.com>
2018-08-25 00:31:29 +0900

    Call onplayerload after load objects

--- Bridge/Presto.SDK.js
+++ Bridge/Presto.SDK.js
@@ -5,6 +5,10 @@ if (!CefSharp) {
 (async function () {
     await CefSharp.BindObjectAsync('player');
     await CefSharp.BindObjectAsync('library');
+
+    if (typeof window.onplayerload === 'function') {
+        window.onplayerload.call();
+    }
 })().catch(e => {
     throw e;
 });
