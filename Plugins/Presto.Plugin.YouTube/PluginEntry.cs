66f280ecc331545cef103abba29ea1310cc1d8a3
IODES <iodesme@gmail.com>
2018-09-03 12:09:38 +0900

    YouTube 플러그인 엔트리 작성

---
+++ Plugins/Presto.Plugin.YouTube/PluginEntry.cs
@@ -0,0 +1,19 @@
+﻿using Presto.Common;
+using Presto.Plugin.YouTube;
+
+[assembly: PrestoTitle("YouTube")]
+[assembly: PrestoAuthor("Kodnix Software")]
+[assembly: PrestoDescription("YouTube 영상을 라이브러리에 추가하는 기능을 제공합니다.")]
+[assembly: PrestoPlugin(typeof(PluginEntry))]
+
+namespace Presto.Plugin.YouTube
+{
+    public class PluginEntry
+    {
+        [PrestoEntry]
+        public static void Initialize()
+        {
+
+        }
+    }
+}
