a34aa59ffe8da367165c1482def73154648c146c
IODES <iodesme@gmail.com>
2018-09-09 03:02:39 +0900

    YouTube 플러그인 태그 기록 구현

--- Plugins/Presto.Plugin.YouTube/ViewModels/AddViewModel.cs
+++ Plugins/Presto.Plugin.YouTube/ViewModels/AddViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Presto.Plugin.YouTube.Dialogs;
 using Presto.Plugin.YouTube.Models;
 using Presto.Plugin.YouTube.Supports;
+using Presto.SDK;
 using System.Collections.Generic;
 using System.Windows.Input;
 
@@ -13,6 +14,8 @@ namespace Presto.Plugin.YouTube.ViewModels
         #endregion
 
         #region 속성
+        public ICommand Add { get; }
+
         public ICommand Cancel { get; }
 
         public IEnumerable<Music> Musics
@@ -29,11 +32,39 @@ namespace Presto.Plugin.YouTube.ViewModels
         #region 생성자
         public AddViewModel()
         {
+            Add = new DelegateCommand(Add_Execute);
             Cancel = new DelegateCommand(Cancel_Execute);
         }
         #endregion
 
         #region 커멘드 함수
+        private void Add_Execute(object obj)
+        {
+            foreach (var music in Musics)
+            {
+                // 태그 정보 작성
+                using (var aacFile = TagLib.File.Create(music.Path))
+                {
+                    aacFile.Tag.Title = music.Title;
+                    aacFile.Tag.Album = music.Album;
+                    aacFile.Tag.AlbumArtists = new[] { music.Artist };
+                    aacFile.Tag.Performers = new[] { music.Artist };
+                    aacFile.Tag.Pictures = new[] { new TagLib.Picture(music.Picture) };
+                    aacFile.Tag.Genres = new[] { music.Genre };
+                    aacFile.Save();
+                }
+
+                // 라이브러리 추가
+                var prestoMusic = PrestoSDK.PrestoService?.Library.LoadMusic(music.Path);
+                if (prestoMusic != null)
+                {
+                    PrestoSDK.PrestoService.Library.AddMusic(prestoMusic);
+                }
+            }
+
+            RaiseCloseRequested();
+        }
+
         private void Cancel_Execute(object obj)
         {
             new SearchDialog().Show();
