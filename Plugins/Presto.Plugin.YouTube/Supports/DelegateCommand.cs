9da0df946cc94169eed4dea9e7795dbb9fe6f274
IODES <iodesme@gmail.com>
2018-09-08 14:32:37 +0900

    Mvvm Support 클래스 추가

---
+++ Plugins/Presto.Plugin.YouTube/Supports/DelegateCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Presto.Plugin.YouTube.Supports
+{
+    public class DelegateCommand : ICommand
+    {
+        #region 상수
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        #endregion
+
+        #region 생성자
+        public DelegateCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+
+        #region ICommand
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+
+            return _canExecute(parameter);
+        }
+        #endregion
+    }
+}
