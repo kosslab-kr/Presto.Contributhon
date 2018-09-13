6fc9886c22db412a49265de062a44acdc348fbc5
mando <nsa06031@gmail.com>
2018-09-13 20:18:33 +0900

    feat: slider click feature

--- Skins/youngdo212/build/js/index.js
+++ Skins/youngdo212/build/js/index.js
@@ -1,20 +1,48 @@
-const playButton = new Vue({
-  el: '#play-button',
-  data: {
-    onPlay: false
-  },
-  computed: {
-    classObject() {
-      return {
-        'player__button': true,
-        'player__button--play': !this.onPlay, 
-        'player__button--pause': !!this.onPlay
-      };
+Vue.component('play-button', {
+  data() {
+    return {
+      onPlay: false
     }
   },
   methods: {
     toggle() {
       this.onPlay = !this.onPlay;
     }
-  }
+  },
+  template: `
+  <div
+    class='player__button'
+    :class='onPlay ? "player__button--pause" : "player__button--play"'
+    @click='toggle'>
+    </div>
+    `
+  })
+  
+Vue.component('player-slider', {
+  data() {
+    return {
+      widthPercentage: 0
+    }
+  },
+  methods: {
+    setPosition({currentTarget, clientX}) {
+      const {width: sliderWidth, left: sliderLeft} = currentTarget.getBoundingClientRect();
+      const positionRatio = ((clientX - sliderLeft) / sliderWidth ) * 100;
+
+      this.widthPercentage = positionRatio;
+    }
+  },
+  template: `
+  <div
+    class="player__slider-wrap"
+    @mousedown='setPosition'>
+    <div class="player__slider">
+      <div
+        :style='{width: widthPercentage + "%"}'
+        class="player__slider-location">
+      </div>
+    </div>
+  </div>`
 })
+
+const player = new Vue({ el: '#player'})
