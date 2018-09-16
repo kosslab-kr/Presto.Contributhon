be477948fdeb34949d11c59a6e878862c49fbe72
JunHoPark93 <pjh374968@gmail.com>
2018-09-16 12:25:51 +0900

    Rename project to NativeSkin.Small

---
+++ Skins/JunHoPark/NativeSkin-small/NativeSkin.Small/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Presto.Common;
+using System;
+using System.Collections.Generic;
+using System.Windows.Interop;
+using System.Windows.Navigation;
+using PCancelEventArgs = Presto.Common.CancelEventArgs;
+
+namespace NativeSkin.Small
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : NavigationWindow, IPrestoWindow
+    {
+        #region IPrestoWindow
+        public IntPtr Handle { get; private set; }
+
+        event EventHandler<PCancelEventArgs> IPrestoWindow.Closing
+        {
+            add => _closingEvents.Add(value);
+            remove => _closingEvents.Remove(value);
+        }
+
+        event EventHandler<EventArgs> IPrestoWindow.Loaded
+        {
+            add => _loadedEvents.Add(value);
+            remove => _loadedEvents.Remove(value);
+        }
+
+        List<EventHandler<PCancelEventArgs>> _closingEvents = new List<EventHandler<PCancelEventArgs>>();
+        List<EventHandler<EventArgs>> _loadedEvents = new List<EventHandler<EventArgs>>();
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Source = new Uri("pack://application:,,,/NativeSkin.Small;component/Home.xaml");
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            Handle = new WindowInteropHelper(this).Handle;
+        }
+    }
+}
---
+++ Skins/JunHoPark/NativeSkin-small/NativeSkin.Small/PlayListView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NativeSkin.Small
+{
+    /// <summary>
+    /// PlayListView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PlayListView : Page
+    {
+        public PlayListView()
+        {
+            InitializeComponent();
+        }
+
+        private void Power_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ExitToApp_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("뒤로 갈 수 없습니다");
+            }
+        }
+    }
+}
