e9c3935b42463760d96448eb92b5fcdecef057a0
JunHoPark93 <pjh374968@gmail.com>
2018-10-15 15:02:23 +0900

    Add ArtistDetail page

---
+++ Skins/JunHoPark/NativeSkin-small/NativeSkin.Small/ArtistDetail.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NativeSkin.Small
+{
+    /// <summary>
+    /// ArtistDetail.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ArtistDetail : UserControl
+    {
+        private MainWindow mainWindow;
+
+        public ArtistDetail(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+    }
+}
