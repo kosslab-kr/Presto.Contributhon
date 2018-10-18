cb44bc512ee80dad1a9b52e7c7abc6485f4d32b7
JunHoPark93 <pjh374968@gmail.com>
2018-10-18 19:31:00 +0900

    Add artist detail model

---
+++ Skins/JunHoPark/NativeSkin-small/NativeSkin.Small/ArtistDetailModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeSkin.Small
+{
+    class ArtistDetailModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public ArtistDetailModel(string title, string content)
+        {
+            this.Title = title;
+            this.Content = content;
+        }
+    }
+}
