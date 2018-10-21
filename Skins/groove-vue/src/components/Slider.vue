79d9620ccaace6225d567db03ee14cc36e609965
Jo Tae Sang <gsts007@gmail.com>
2018-10-21 09:35:59 +0900

    Add touch events to slider

--- Skins/groove-vue/src/components/Slider.vue
+++ Skins/groove-vue/src/components/Slider.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="Slider__Wrapper" @mousedown="onMouseDown">
+    <div class="Slider__Wrapper" @mousedown="onMouseDown" @touchstart="onTouchStart">
         <div class="slider" ref="slider">
             <div class="slider-line" :style="{ width: `${percent}%` }"></div>
         </div>
@@ -38,20 +38,42 @@
         },
 
         methods: {
+            touchFromMouse (e) {
+                if (e.targetTouches.length) {
+                    return e.targetTouches[0];
+                }
+
+                if (e.touches.length) {
+                    return e.touches[0];
+                }
+
+                return e.changedTouches[0];
+            },
+
             updatePosition(e) {
-                let x = e.target.offsetLeft + e.offsetX - this.$refs.slider.offsetLeft;
+                let x = e.pageX - this.$refs.slider.offsetLeft;
                 let width = this.$refs.slider.offsetWidth;
 
                 let percent = Math.min(Math.max(0, x / width), 1);
                 this.currentValue = this.max * percent;
             },
 
-            onDocumentMouseUp (e) {
-                if (!this.isSeeking) return;
-                this.updatePosition(e);
+            onMouseDown (e) {
+                e.preventDefault();
 
-                this.startX = null;
-                this.$emit('end', this.currentValue);
+                this.updatePosition(e);
+                this.$emit('start', this.currentValue);
+                this.isSeeking = true;
+            },
+
+            onTouchStart (e) {
+                e.preventDefault();
+                let mouse = this.touchFromMouse(e);
+                console.log(mouse);
+
+                this.updatePosition(mouse);
+                this.$emit('start', this.currentValue);
+                this.isSeeking = true;
             },
 
             onDocumentMouseMove (e) {
@@ -61,12 +83,32 @@
                 this.updatePosition(e);
             },
 
-            onMouseDown (e) {
+            onDocumentTouchMove (e) {
+                if (!this.isSeeking) return;
+
+                let mouse = this.touchFromMouse(e);
+                this.updatePosition(mouse);
+            },
+
+            onDocumentMouseUp (e) {
+                if (!this.isSeeking) return;
                 e.preventDefault();
-                this.startX = e.pageX;
 
                 this.updatePosition(e);
-                this.$emit('start', this.currentValue);
+
+                this.$emit('end', this.currentValue);
+                this.isSeeking = false;
+            },
+
+            onDocumentTouchEnd (e) {
+                if (!this.isSeeking) return;
+                e.preventDefault();
+
+                let mouse = this.touchFromMouse(e);
+                this.updatePosition(mouse);
+
+                this.$emit('end', this.currentValue);
+                this.isSeeking = false;
             },
         },
 
@@ -75,27 +117,29 @@
                 if (this.max === 0) return 0;
                 return this.currentValue / this.max * 100;
             },
-
-            isSeeking () {
-                return this.startX !== null;
-            },
         },
 
         data () {
             return {
                 currentValue: this.value,
-                startX: null,
+                isSeeking: false,
             };
         },
 
         created () {
             document.addEventListener('mousemove', this.onDocumentMouseMove);
             document.addEventListener('mouseup', this.onDocumentMouseUp);
+
+            document.addEventListener('touchmove', this.onDocumentTouchMove);
+            document.addEventListener('touchend', this.onDocumentTouchEnd);
         },
 
         destroyed () {
             document.removeEventListener('mousemove', this.onDocumentMouseMove);
             document.removeEventListener('mouseup', this.onDocumentMouseUp);
+
+            document.removeEventListener('touchmove', this.onDocumentTouchMove);
+            document.removeEventListener('touchend', this.onDocumentTouchEnd);
         },
     };
 </script>
